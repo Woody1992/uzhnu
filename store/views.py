206d7cd0588174cbd0fbd6aac87e2af610dbb177
yuri <yuri.yanochik@pettersonapps.com>
2022-04-15 11:27:30 +0300

    added DB and endpoint for listing all the products in the store

--- store/views.py
+++ store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Product
 
-# Create your views here.
+class ProductView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        products = Product.objects.all()
+        context = {
+            'products': products,
+        }
+        return render(request, "products/product_list.html", context)
